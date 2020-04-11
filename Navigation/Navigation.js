c5f78aa8fe4565d0593ce15818ec4a9022d567ab
Matthieu Kirschleger <mat.kirsh@gmail.com>
2020-04-11 23:27:21 +0200

    modif

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import {StyleSheet, Image} from 'react-native'
 import {createStackNavigator} from 'react-navigation-stack'
 import {createAppContainer} from 'react-navigation'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
 import Search from '../Components/Search'
 import FilmDetail from '../Components/FilmDetail'
+import Favorites from '../Components/Favorites'
 
 const SearchStackNavigator = createStackNavigator({
   Search: {
@@ -15,4 +19,45 @@ const SearchStackNavigator = createStackNavigator({
   }
 })
 
-export default createAppContainer(SearchStackNavigator)
+const MoviesTabNavigator = createBottomTabNavigator ({
+  Search: {
+    screen : SearchStackNavigator,
+    navigationOptions: {
+      tabBarIcon: () => {
+        return <Image
+        source = {require('../Images/ic_search.png')}
+        style = {styles.icon}
+        />
+      }
+    }
+  },
+  Favorites: {
+    screen: Favorites,
+    navigationOptions: {
+      tabBarIcon: () => {
+        return <Image
+        source = {require('../Images/ic_favorite.png')}
+        style = {styles.icon}
+        />
+      }
+    }
+  }
+},
+  {
+    tabBarOptions: {
+      activeBackgroundColor: '#DDDDDD', //couleur arrière-plan onglet sélectionné
+      inactiveBackgroundColor: '#FFFFFF', //couleur background onglet non sélectionné
+      showLabel: false, //ne pas montrer le nom des onglets
+      showIcon: true //montrer l'icône de l'onglet
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default createAppContainer(MoviesTabNavigator)
